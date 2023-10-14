d98a34d8f17fc7c73df64078f6e2f0422a10db4a
JHoangUC <jhoanguc@gmail.com>
2023-10-13 20:13:41 -0700

    Adding preview image in newItem
    Changed newItem.js to show a preview of the selected image file through local storage

--- public/js/newItem.js
+++ public/js/newItem.js
@@ -10,8 +10,17 @@ function readURL(e) {
     if (this.files && this.files[0]) {
         console.log('im here');
         var reader = new FileReader();
-        $(reader).load(function(e) { $('#img').attr('src', e.target.result); });
+
+        $(reader).on('load', function(e) { 
+          localStorage.setItem("recent-image", e.target.result);  
+          const recentImage = localStorage.getItem("recent-image")
+          $('#preview').attr('src', recentImage); 
+          $('#img').attr('src', e.target.result); 
+          
+        });
+
         reader.readAsDataURL(this.files[0]);
     }
 }
+
 $("#fileStuff").change(readURL);
